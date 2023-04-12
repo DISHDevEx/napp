afa5b61ccb6f5679a079a76379aab3c620a9c3de
vinnysharm <vinayak.sharma@dish.com>
2023-04-12 21:19:55 +0000

    fixing pylint issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,15 @@ that can be reused throughout pytest without redefinition.
 It also defines the configurations for pytest.
 """
 
-import json
 import pathlib
 import pytest
 
 
 @pytest.fixture
 def test_case_json(request):
+    """
+    Fixture that allows a static json file to emulate test case json that is used in traffic simulator. 
+    """
     file = pathlib.Path(request.node.fspath.strpath)
     test_case_json = file.with_name("static_test_case_values.json")
     return test_case_json
